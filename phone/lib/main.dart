4a63efda15a50a66b509bc8838050a02bd8cdfb2
Taqi Hamoda <taqihisham@gmail.com>
2020-01-01 16:24:16 -0500

    Played a little with the login

--- phone/lib/main.dart
+++ phone/lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 
-void main() => runApp(Trainer());
+void main() => runApp(Starter());
 
-class Trainer extends StatelessWidget { // Trainer is a placeholder name for the app name.
+class Starter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color(0xFF7775FF), // eh ra2yak?
-        scaffoldBackgroundColor: Color(0xFF7775FF),
-      ),
       home: LoginPage(),
     );
   }
--- phone/lib/screens/login_screen.dart
+++ phone/lib/screens/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:phone/screens/register_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'director_screen.dart';
 import 'trainer_screen.dart';
 import 'user_screen.dart';
 import 'forgot_password_screen.dart';
+
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,141 +16,127 @@ class _LoginPageState extends State<LoginPage> {
 
   String userEmail = '';
   String userPassword = '';
-  String userType = ''; // user? trainer? director? etc. Will go more in depth once we use firebase.
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text(
-              'Trainer',
-              style: TextStyle(
-                color: Color(0xFF292E21),
-                fontFamily: 'Boogaloo',
-                fontSize: 45.0,
+      body: Container(
+        decoration: BoxDecoration(image: DecorationImage(
+            image: AssetImage('images/background1.jpg'), fit: BoxFit.cover),),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: <Widget>[
+
+            Center(
+              child: Image.asset('images/temp-logo.png',
+              height: 200,
+              width: 200,)
+            ),
+
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 5.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.8),
+                  icon: Icon(
+                    Icons.mail,
+                    color: Colors.white,
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+                onChanged: (value){
+                  userEmail = value;
+                },
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextField(
-              style: TextStyle(
-                color: Colors.black,
+
+            Container(
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0, bottom: 10.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.8),
+                  icon: Icon(
+                    Icons.lock,
+                    color: Colors.white,
+                  ),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
+                    ),
+                  ),
+                ),
+                onChanged: (value){
+                  userPassword = value;
+                },
               ),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                icon: Icon(
-                  Icons.mail,
-                  color: Colors.white,
-                ),
-                hintText: 'Email',
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.0),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 200.0),
+              child: FlatButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  ForgotPwPage()));
+                },
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white
                   ),
                 ),
               ),
-              onChanged: (value){
-                userEmail = value;
-              },
             ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextField(
-              style: TextStyle(
-                color: Colors.black,
-              ),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                icon: Icon(
-                  Icons.lock,
-                  color: Colors.white,
-                ),
-                hintText: 'Password',
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10.0),
+
+            ButtonTheme(
+              minWidth: 250.0,
+              height: 20.0,
+              buttonColor: Colors.lightBlue,
+              child: RaisedButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  UserPage()));
+                  // for now, eventually will have bunch of if statements to determine user type.
+                },
+                child: Text(
+                  'Log-in',
+                  style: TextStyle(
+                    fontFamily: 'RobotoCondensed',
+                    color: Colors.white,
+                    fontSize: 30.0,
                   ),
                 ),
-              ),
-              onChanged: (value){
-                userPassword = value;
-              },
-            ),
-          ),
-          ButtonTheme(
-            minWidth: 275.0,
-            height: 20.0,
-            buttonColor: Colors.orangeAccent,
-            child: RaisedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>  RegisterPage()));
-              },
-              child: Text(
-                'Register',
-                style: TextStyle(
-                  color: Color(0xFF292E21),
-                  fontFamily: 'Boogaloo',
-                  fontSize: 35.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0),
                 ),
               ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
             ),
-          ),
-          ButtonTheme(
-            minWidth: 275.0,
-            height: 20.0,
-            buttonColor: Colors.orangeAccent,
-            child: RaisedButton(
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>  UserPage()));
-                // for now, eventually will have bunch of if statements to determine user type.
-              },
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Color(0xFF292E21),
-                  fontFamily: 'Boogaloo',
-                  fontSize: 35.0,
-                ),
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-            ),
-          ),
-          FlatButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>  ForgotPwPage()));
-            },
-            child: Text(
-              'Forgot Password?',
-              style: TextStyle(
-                fontSize: 17.5,
-              ),
-            ),
-          ),
-        ],
+
+          ],
+        ),
       ),
     );
   }
