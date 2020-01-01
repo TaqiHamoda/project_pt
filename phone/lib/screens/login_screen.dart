c5b3bd23fce6536dba11d8029847ae7ce3a5f164
Taqi Hamoda <taqihisham@gmail.com>
2020-01-01 17:07:17 -0500

    Fixed the login

--- phone/lib/screens/login_screen.dart
+++ phone/lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:phone/screens/register_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -6,14 +7,12 @@ import 'trainer_screen.dart';
 import 'user_screen.dart';
 import 'forgot_password_screen.dart';
 
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   String userEmail = '';
   String userPassword = '';
 
@@ -21,23 +20,23 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(image: DecorationImage(
-            image: AssetImage('images/background1.jpg'), fit: BoxFit.cover),),
-
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/background1.jpg'), fit: BoxFit.cover),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-
           children: <Widget>[
-
             Center(
-              child: Image.asset('images/temp-logo.png',
+                child: Image.asset(
+              'images/temp-logo.png',
               height: 200,
-              width: 200,)
-            ),
-
+              width: 200,
+            )),
             Container(
-              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0, bottom: 5.0),
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 10.0, bottom: 5.0),
               child: TextField(
                 style: TextStyle(
                   color: Colors.black,
@@ -60,14 +59,14 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ),
-                onChanged: (value){
+                onChanged: (value) {
                   userEmail = value;
                 },
               ),
             ),
-
             Container(
-              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0, bottom: 10.0),
+              padding: EdgeInsets.only(
+                  left: 20.0, right: 20.0, top: 5.0, bottom: 10.0),
               child: TextField(
                 style: TextStyle(
                   color: Colors.black,
@@ -90,51 +89,56 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ),
-                onChanged: (value){
+                onChanged: (value) {
                   userPassword = value;
                 },
               ),
             ),
-
             Padding(
               padding: const EdgeInsets.only(left: 200.0),
               child: FlatButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  ForgotPwPage()));
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ForgotPwPage()));
                 },
                 child: Text(
                   'Forgot Password?',
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white
+                  style: TextStyle(fontSize: 15, color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              width: 250.0,
+              height: 50.0,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue[50].withOpacity(0.1),
+                border: Border(
+                  top: BorderSide(width: 2.0, color: Colors.lightBlue.shade50),
+                  bottom: BorderSide(width: 2.0, color: Colors.lightBlue.shade50),
+                  right: BorderSide(width: 2.0, color: Colors.lightBlue.shade50),
+                  left: BorderSide(width: 2.0, color: Colors.lightBlue.shade50)
+                ),
+
+              ),
+
+              child: Center(
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => UserPage()));
+                    // for now, eventually will have bunch of if statements to determine user type.
+                  },
+                  child: Text('Log-in',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'RobotoCondensed',
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
                   ),
                 ),
               ),
             ),
-
-            ButtonTheme(
-              minWidth: 250.0,
-              height: 20.0,
-              buttonColor: Colors.lightBlue,
-              child: RaisedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  UserPage()));
-                  // for now, eventually will have bunch of if statements to determine user type.
-                },
-                child: Text(
-                  'Log-in',
-                  style: TextStyle(
-                    fontFamily: 'RobotoCondensed',
-                    color: Colors.white,
-                    fontSize: 30.0,
-                  ),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(9.0),
-                ),
-              ),
-            ),
-
           ],
         ),
       ),
