cdd2a8ad8975f51487596121f40c2ed9d9ce259e
Taqi Hamoda <taqihisham@gmail.com>
2020-01-02 19:53:56 -0500

    Fixed the issues with the login and added some expanded widgets

--- phone/lib/screens/local_users.dart
+++ phone/lib/screens/local_users.dart
@@ -1,10 +1,1 @@
-import 'users.dart';
 
-List<User> users = [];
-
-void createUsers(){
-  Trainer trainer = Trainer('Taqi', 'Hamoda', 'taqi@app.com', 'passwordbb', '1234567890');
-  Client client = Client('Bob', 'James', 'bob@app.com', 'Bebop', '1234567890', trainer);
-
-  users.addAll([trainer, client]);
-}
